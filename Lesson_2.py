e0d54ea5bf37d73f496e64d5d73966d7bedd83d6
akaKAIN <mkain@mail.ru>
2019-03-14 07:14:59 +1000

    Добавил в программу ветвление ELIF

--- Lesson_2.py
+++ Lesson_2.py
@@ -6,5 +6,7 @@ name = input('Как Ваше имя?  ')
 answer = input('Рад видеть Вас, {} желаете закоммитить ченить,  Y/N?  '.format(name))
 if answer.lower() == 'y':
 	print('Excellent! Ни дня без кода!')
-else:
+elif answer.lower() == 'n':
 	print('Жаль ... теряешь время')
+else:
+	print('Что-то? ...')
