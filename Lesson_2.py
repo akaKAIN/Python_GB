43fbab3726c3d979419e8231590fb6b5ed7b7807
akaKAIN <mkain@mail.ru>
2019-03-14 08:06:35 +1000

    Дописал программу. Добавил в нее ДЗ второго урока

--- Lesson_2.py
+++ Lesson_2.py
@@ -6,6 +6,27 @@ name = input('Как Ваше имя?  ')
 answer = input('Рад видеть Вас, {} желаете закоммитить ченить,  Y/N?  '.format(name))
 if answer.lower() == 'y':
 	print('Excellent! Ни дня без кода!')
+	choise = input(
+		'''Введите номер пункта меню для выбора:
+		1. Запустить Python
+		2. Запустить диспетчер задач
+		3. Запустить командную строку
+		4. Уйти за кофем\n''')
+	if choise == '1':
+		print('Запускаем Python ...\n\n')
+		input()
+	elif choise == '2':
+		print('Диспетчер в пути ...\n\n')
+		input()
+	elif choise == '3':
+		print('Ищем командную строку\n\n')
+		input()
+	elif choise == '4':
+		print('Кофе пьем, коды бьем!\n\n')
+		input()
+	else:
+		print('Я Вас опять не понимаю.\n\n')
+
 elif answer.lower() == 'n':
 	print('Жаль ... теряешь время')
 else:
