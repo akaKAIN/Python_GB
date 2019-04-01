1ec4511cd00955d06d96cece02d6738b5943817e
akaKAIN <mkain@mail.ru>
2019-04-01 19:17:29 +1000

    Раскоментировал Задачу №2

--- HW_02/HW_02_Hard.py
+++ HW_02/HW_02_Hard.py
@@ -22,19 +22,19 @@ print(f'y = {int(k)} * {x} + {b} = {int(k) * x + float(b)}')
 # 4. Длина исходной строки для частей должна быть в соответствии с форматом
 #  (т.е. 2 символа для дня, 2 - для месяца, 4 - для года)
 
-# day_in_month = 30
-# day, month, year = input('Введите дату (в формате дд.мм.гггг): ').split('.')
-# if len(day) == 2 and len(month) == 2 and len(year) == 4:
-#     if 1 <= int(month) <= 12 and 1 <= int(year) <= 9999:
-#         if (int(month) <= 7 and int(month) % 2 != 0) or \
-#                 (int(month) >= 8 and int(month) % 2 == 0):
-#             day_in_month = 31
-#     if 1 <= int(day) <= day_in_month:
-#         print('Дата введена корректно.')
-#     else:
-#         print('Введенные данные больше/меньше возможных.')
-# else:
-#     print('Введенные данные не соответствуют формату дд.мм.гггг')
+day_in_month = 30
+day, month, year = input('Введите дату (в формате дд.мм.гггг): ').split('.')
+if len(day) == 2 and len(month) == 2 and len(year) == 4:
+    if 1 <= int(month) <= 12 and 1 <= int(year) <= 9999:
+        if (int(month) <= 7 and int(month) % 2 != 0) or \
+                (int(month) >= 8 and int(month) % 2 == 0):
+            day_in_month = 31
+    if 1 <= int(day) <= day_in_month:
+        print('Дата введена корректно.')
+    else:
+        print('Введенные данные больше/меньше возможных.')
+else:
+    print('Введенные данные не соответствуют формату дд.мм.гггг')
 
 
 
@@ -70,6 +70,7 @@ cut_block = block_of_rooms[:(block_of_rooms.index(room) + 1)]
 result_floor = floor_begin_of_last_block + math.ceil(len(cut_block) / x)
 
 # Костыль наверное, но времени уже не осталось.
+# Чтобы вместо номеров кратным х не ставились нули.
 if len(cut_block) % x == 0:
     result_num = x
 else:
