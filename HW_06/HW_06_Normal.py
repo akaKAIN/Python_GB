515766049528b6815ed62ee38c041aae05204b0e
akaKAIN <mkain@mail.ru>
2019-04-14 15:40:49 +1000

    Правки классов

--- HW_06/HW_06_Normal.py
+++ HW_06/HW_06_Normal.py
@@ -13,8 +13,8 @@
 import random
 
 
-class Character:
-    def __init__(self, name='GM', health=120, damage=20, armor=1.5):
+class Person:
+    def __init__(self, name=None, health=100, damage=15, armor=1.2):
         self.name = name
         self.health = health
         self.damage = damage
@@ -36,9 +36,25 @@ class Character:
         other.health -= cl_damage
         print(f'Здоровье несчастного опустилось до {other.health}\n')
 
+class Player(Person):
+    def __init__(self, name, damage=15, health=120, armor=1.5):
+        super().__init__(Person)
+        self.name = name
+        self.armor = armor
+        self.damage = damage
+        self.health = health
 
-enemy = Character('Enemy', 100, 15, 1.2)
-player = Character(input('Enter name of character: '), 100, 15, 1.2)
+class Enemy(Person):
+    def __init__(self, name='Enemy', damage=20, health=100):
+        super().__init__(Person)
+        self.name = name
+        self.damage = damage
+        self.health = health
+
+
+enemy = Enemy()
+player = Player('Player')
+# print(f'player.health={player.health}, enemy.health={enemy.health}')
 
 while enemy.health > 0 and player.health > 0:
     if random.randrange(1, 3) % 2 == 0:
