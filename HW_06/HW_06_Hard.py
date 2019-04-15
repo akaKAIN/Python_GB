a10efedacea7b822122b214f44778544c544feb6
akaKAIN <mkain@mail.ru>
2019-04-15 14:27:51 +1000

    Выполнил ДЗ№06. Сложность - "тяжелая"

--- HW_06/HW_06_Hard.py
+++ HW_06/HW_06_Hard.py
@@ -21,44 +21,88 @@
 
 import random
 
-class ToyFactory:
-    def __init__(self):
-        self.purchase()
-        self.crafting()
-        self.painting()
-
-    def purchase(self):
-        print('Запукаем материал')
-
-    def crafting(self):
-        print('Шьем игрушку по схемам')
-
-    def painting(self):
-        print('Окрашиваем игрушку')
-
-class Toy:
-    def __init__(self):
-        self.name = random.choice(toys_names)
-        self.toy_type = random.choice(toys_types)
-        self.color = random.choice(toys_color)
-
-    def label(self):
-        print(f'{self.color} {self.toy_type} по имени {self.name}')
-
 
 toys_types = [
-    'охотник', 'волк', 'пингвин', 'крот',
+    'охотник', 'волк', 'опосум', 'котейка',
     'мутант', 'Питон', 'носорог', 'тигр'
 ]
 toys_names = [
-    'Элвин', 'Свистун', 'Ходор', 'Луи',
-    'Хьюго', 'Шуршун', 'Дэйв', 'Иван'
+    'Элвин', 'Свистун', 'Ходор', 'Шпуньк',
+    'Хьюго', 'Шуршун', 'Арнольд', 'Иван'
 ]
 toys_color = [
     'желтый', 'синий', 'черный', 'белый',
-    'красный', 'лиловый в желтую крапинку',
+    'красный', 'хмурый', 'одинокий',
     'прозрачный', 'мутный', 'сизый'
 ]
+soft_toys = [
+    'плюшевый медвепут', 'плюшевый Гейб',
+    'плюшевый змей', 'плюшевый огурец'
+]
+person_toys = [
+    'капитан', 'царь', 'учитель', 'плут', 'дурачок'
+]
 
-a = ToyFactory()
-print(a)
+
+class Toy:
+    def __init__(self, name, color, toy_type):
+        self.name = name
+        self.color = color
+        self.toy_type = toy_type
+
+    @property
+    def label(self):
+        return print(f'\nПеред Вами игрушка:\n{self.color} {self.toy_type} по имени {self.name}')
+
+
+class SoftToy(Toy):
+    def __init__(self, name, color, toy_type=random.choice(soft_toys)):
+        Toy.__init__(self, name, color, toy_type)
+        self.toy_type = toy_type
+
+class PersonToy(Toy):
+    def __init__(self, name, color, toy_type=random.choice(person_toys)):
+        Toy.__init__(self, name, color, toy_type)
+        self.toy_type = toy_type
+
+
+
+class ToyFactory(Toy):
+    def __init__(self,
+                 name=random.choice(toys_names),
+                 color=random.choice(toys_color),
+                 toy_type=random.choice(toys_types)
+                 ):
+        Toy.__init__(self, name, color, toy_type)
+        self.name = name
+        self.color = color
+        self.toy_type = toy_type
+        self.__purchase
+        self.__crafting
+        self.__painting
+
+    def make_soft_toy(self):
+        return SoftToy(self.name, self.color)
+
+    def make_person_toy(self):
+        return PersonToy(self.name, self.color)
+
+
+    @property
+    def __purchase(self):
+        return print('Закупаем материал')
+
+    @property
+    def __crafting(self):
+        return print('Изготвливаем игрушку по шаблону')
+
+    @property
+    def __painting(self):
+        return print('Окрашиваем игрушку')
+
+
+crafting_toy = ToyFactory()
+crafting_toy.label
+
+a = crafting_toy.make_soft_toy()
+a.label
